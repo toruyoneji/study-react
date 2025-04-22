50561f66bcf5486c8591b10feee4dd9a568b13c8
toruyoneji <toruyoneji0514@icloud.com>
2025-04-23 02:31:36 +0900

    💄 css style moduleのコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "@/components/Headline.module.css";
 
 
 
@@ -10,10 +10,10 @@ export function Headline(props) {
     
   return (
     <div>   
-          <h1>{props.page} Page</h1>
+          <h1 className={styles.h1}>{props.page} Page</h1>
           <ol>
             <li>
-              Get started by editing <code>pages/{props.page}.js</code>.
+              Get started by editing <code className={styles.code}>pages/{props.page}.js</code>.
             </li>
             {props.children}
           </ol>
